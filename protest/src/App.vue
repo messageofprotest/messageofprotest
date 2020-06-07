125af312cb6285d724f6f42c774e82abe81797ce
Nathan Smith <nathansforyou@gmail.com>
2020-06-06 23:12:11 -0400

    Initial commit, civics api and representative cards

---
+++ protest/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+// import Protest from './components/Protest';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
