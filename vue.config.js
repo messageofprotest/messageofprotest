f282516a8294fbff0c2c70420a3712b14c93985c
= <dominic.napoleon@gmail.com>
2020-06-13 18:46:39 -0400

    remove webpack mods #12

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = {
 }
 
 // configuring webpack publicpath temporarily, when we get a root level domain we can remove this #12
-if(process.env.GITHUB_ACTIONS) {
-    module.exports.publicPath = 'yay/'
-}
+// if(process.env.GITHUB_ACTIONS) {
+//     module.exports.publicPath = 'yay/'
+// }
