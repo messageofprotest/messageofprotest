336eb757c63ae440b12e3510a2886faef238f720
= <dominic.napoleon@gmail.com>
2020-06-13 21:36:58 -0400

    builds now deploy to yayboys.github.io master branch #12

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,3 @@ module.exports = {
   // disable the linter
   lintOnSave: false,
 }
-
-// configuring webpack publicpath temporarily, when we get a root level domain we can remove this #12
-// if(process.env.GITHUB_ACTIONS) {
-//     module.exports.publicPath = '/yay/'
-// }
