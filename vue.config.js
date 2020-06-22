8b327ec355ef8d110ad77a8c4b56474f4634405a
= <dominic.napoleon@gmail.com>
2020-06-22 15:05:03 -0400

    add relative pathing to webpack build config for staging server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   // disable the linter
   lintOnSave: false,
+
+  // we serve our staging server at relative path, at messageofprotest.com/staging
+  // we need to set the publicPath so webpack generated resources have correct paths
+  publicPath: process.env.SERVER === 'staging'
+    ? '/staging/' : '/',
 }
